8f20c84191d7b8d80354bf5ec1411b275658f5e3
Nguyễn Lê Trung Kiên <thanheco1978@gmail.com>
2025-01-04 22:47:49 +0700

    new bai

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,10 +6,20 @@ class MyComponent extends React.Component {
     adress: "Hoi dan IT",
     age: 26,
   };
+
+  onClick1 = (event) => {
+    console.log(event);
+    console.log("  My name is ", this.state.name);
+
+    this.setState({
+      name: "Eric",
+    });
+  };
   render() {
     return (
       <div>
         My name is {this.state.name} and i from {this.state.adress}
+        <button onClick={this.onClick1}>Click me</button>
       </div>
     );
   }
